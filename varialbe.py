261c5cb0bda6b2dbd4069414c6a6e6e6217cea9c
이정현 <spsapientas@gmail.com>
2025-08-09 11:11:28 +0900

    기본 데이터 형태 학습

---
+++ varialbe.py
@@ -0,0 +1,28 @@
+# x = "안녕"
+# print(x)
+# print(type(x))
+#
+# x = 3
+# print(x)
+# print(type(x))
+#
+# x = 3 + 5
+# print(x)
+# print(type(x))
+#
+# x = 3 == 5
+# print(x)
+# print(type(x))
+#
+# x = True
+# print(x)
+# print(type(x))
+
+x = [1,2,3,4,5,5,6,"good morning",66,7] #list
+print(x)
+print(type(x))
+
+x = {'a' : 1, 'b' : 5} #dictionary  key : value
+print(x)
+print(x['a'])
+print(type(x))

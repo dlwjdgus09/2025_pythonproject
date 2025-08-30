d898cf27f4242dcb9526c042ecb0e5434bed27f9
이정현 <spsapientas@gmail.com>
2025-08-30 09:31:34 +0900

    숫자 형태의 연산

---
+++ 2. operater.py
@@ -0,0 +1,34 @@
+# 대입 연산자
+x = 6
+y = 4
+
+# 조건 연산자
+z = x == y
+print("x == y : ", z)
+z = x != y
+print("x != y : ", z)
+z = x > y
+print("x > y : ", z)
+z = x >= y
+print("x >= y : ", z)
+z = x < y
+print("x < y : ", z)
+z = x <= y
+print("x <= y : ", z)
+
+z = x + y
+print("x + y", z)
+z = x - y
+print("x - y", z)
+z = x * y
+print("x * y", z)
+z = x / y
+print("x / y", z)
+
+z = x % y
+print("x % y", z)
+z = x // y
+print("x // y", z)
+
+z = x ** y
+print("x ** y", z)

a089d422267129a45061ebcd33b9520d3fd25ef0
이정현 <spsapientas@gmail.com>
2025-11-22 11:46:47 +0900

    문제 제출

--- 3.control.statement.py
+++ 3.control.statement.py
@@ -66,37 +66,78 @@ def repeat_function():
 # for i in arr:
 #     print(i)
 
-# 수학
-test = [{'answer': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]},
- {'answer': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
- {'answer': [1, 5, 3, 4, 5, 1, 2, 3, 1, 2]},
- {'answer': [4, 3, 4, 4, 5, 3, 1, 2, 2, 4]},
- {'answer': [1, 3, 4, 4, 5, 3, 1, 2, 3, 2]},
- {'answer': [1, 3, 5, 4, 5, 3, 1, 2, 3, 4]},]
+test = [
+    {
+        'name': 'aaa',
+        'number': 10101,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'bbb',
+        'number': 10102,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ccc',
+        'number': 10103,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+  {
+        'name': 'ddd',
+        'number': 10104,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'eee',
+        'number': 10105,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    }
+]
 
-a = [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]
-correct_answer = [1, 3, 2, 4, 5, 3, 1, 2, 3, 4]
+correct_answer = {
+    'math':[1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'korean':[1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'english':[1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'science':[1, 3, 2, 4, 5, 3, 1, 2, 3, 4]
+}
+
 
 # 학생의 점수 구하기
-#풀이1
-score = 0
-for (student, correct) in zip(a, correct_answer):
-    print(student, '/', correct)
-    if student == correct:
-        score = score + 10
-print(score)
+def get_score(student_answer, correct_answer):
+    score = 0
+    for (student, correct) in zip(student_answer, correct_answer):
+        #print(student, '/', correct)
+        if student == correct:
+            score = score + 10
+    #print(score)
+    return score
 
-#풀이2
-# score = 100
-# for (student, correct) in zip(a, correct_answer):
-#     print(student, '/', correct)
-#     if student != correct:
-#         score = score - 10
-# print(score)
-#
-# #풀이3
-# score = 0
-# for i in range(len(a)):
-#     if a[i] == correct_answer[i]:
-#         score = score + 10
-# print(score)
+for student in test:
+    if student['name'] == 'ccc': # 배열 반복에서 스킵하는 방법
+        continue
+
+    print("학생", student['name'], "==================")
+    math_score = get_score(student['math'], correct_answer['math'])
+    korean_score = get_score(student['korean'], correct_answer['korean'])
+    english_score = get_score(student['english'], correct_answer['english'])
+    science_score = get_score(student['science'], correct_answer['science'])
+
+    print("수학점수:", math_score)
+    print("국어점수:", korean_score)
+    print("영어점수:", english_score)
+    print("과학점수:", science_score)

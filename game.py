fe99826f7aa2850b8dd394147a21fa9c84603ae6
이정현 <spsapientas@gmail.com>
2026-02-21 10:33:14 +0900

    STEP 1 중반

--- game.py
+++ game.py
@@ -1,9 +1,25 @@
+import random
+
 print("게임에 오신 것을 환영합니다!")
+weapon_level = 0
 while True:
+    print("1. 무기 강화")
+    print("0. 종료하기")
     choice = input("숫자를 입력하세요 (0을 입력하면 종료):")
     if choice == "0":
         print("게임을 종료합니다.")
         break
-    else:
-        continue
-
+    upgrade_rates = [
+        {"up": 70, "keep": 30, "down": 0, "break": 0},
+        {"up": 60, "keep": 25, "down": 10, "break": 5},
+        {"up": 50, "keep": 30, "down": 15, "break": 5},
+        {"up": 45, "keep": 30, "down": 20, "break": 5},
+        {"up": 40, "keep": 30, "down": 20, "break": 10},
+        {"up": 35, "keep": 30, "down": 25, "break": 10},
+        {"up": 30, "keep": 30, "down": 30, "break": 10},
+        {"up": 25, "keep": 30, "down": 30, "break": 15},
+        {"up": 20, "keep": 30, "down": 30, "break": 20},
+        {"up": 15, "keep": 30, "down": 30, "break": 25},
+        {"up": 0, "keep": 100, "down": 0, "break": 0}
+    ]
+    ran_num = random.randint(0, 99)
